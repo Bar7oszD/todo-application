2c7edb6499418791c141506c7ec8fd7335f09e04
Bartosz Długosz <dbartek2016@gmail.com>
2025-03-31 22:30:46 +0200

    Frontend

--- src/main/java/io/github/Bar7oszD/todo/TodoRepository.java
+++ src/main/java/io/github/Bar7oszD/todo/TodoRepository.java
@@ -3,7 +3,7 @@ package io.github.Bar7oszD.todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 interface TodoRepository extends JpaRepository<Todo,Integer> {
 }
 
